decd4f19b95ac7f83cb660137e37c8a3a2c8de46
Bruno Binet <bruno.binet@gmail.com>
2022-07-06 18:27:41 +0200

    Remove flask support and werkzeug dependency

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
-# multipart-upload-server
-multipart/form-data upload server for Flask &amp; Tornado
-
-You can choose the one you like.
-
-# Using Tornado
+# tornado-upload
+multipart/form-data upload server for Tornado
 
 ## Install Tornado
 ```
@@ -24,44 +20,6 @@ python upload.py
 ```
 curl -X POST -F upfile=@tornado/tornado-web-service.jpg http://localhost:8080/upload_multipart
 
-ls -l tornado/UPLOADED/
+ls -l tornado/uploaded/
 -rw-rw-r-- 1 nop nop 13189  5月 21 08:58 tornado-web-service.jpg
 ```
-
----
-
-# Using Flask
-
-## Install Flask
-```
-sudo apt install python3-pip python3-setuptools python3-magic
-python -m pip install -U pip
-python -m pip install -U wheel
-python -m pip install -U Werkzeug
-python -m pip install flask
-```
-
-## Start WEB Server using Flask
-```
-git clone https://github.com/nopnop2002/multipart-upload-server
-cd multipart-upload-server/flask
-python upload.py
-```
-
-## Upload file using curl
-```
-curl -X POST -F upfile=@flask/Flask_logo.png http://localhost:8080/upload_multipart
-
-ls -l flask/UPLOADED/
--rw-rw-r-- 1 nop nop 13189  5月 21 08:58 Flask_logo.png
-```
-
----
-
-# View file
-![multipart-upload-server-1](https://user-images.githubusercontent.com/6020549/119225534-4af78000-bb3f-11eb-83fc-d3c93b31e4eb.jpg)
-
-![multipart-upload-server-2](https://user-images.githubusercontent.com/6020549/119225542-5054ca80-bb3f-11eb-95a3-f558e606f68c.jpg)
-
-![multipart-upload-server-3](https://user-images.githubusercontent.com/6020549/119225537-4b901680-bb3f-11eb-9f0c-e009b5f6c56d.jpg)
-
--- tornado/upload.py
+++ tornado/upload.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
-import werkzeug
 from datetime import datetime
 import tornado.httpserver
 import tornado.ioloop
@@ -10,8 +9,11 @@ import tornado.web
 import logging
 import json
 import magic
+import re
+import unicodedata
 
 from tornado.options import define, options
+
 define("port", default=8080, help="run on the given port", type=int)
 
 UPLOAD_DIR = os.path.join(os.getcwd(), "uploaded")
@@ -19,6 +21,37 @@ logging.info("UPLOAD_DIR={}".format(UPLOAD_DIR))
 if (os.path.exists(UPLOAD_DIR) == False):
 	logging.warning("UPLOAD_DIR [{}] not found. Create this".format(UPLOAD_DIR))
 	os.mkdir(UPLOAD_DIR)
+
+def secure_filename(filename: str) -> str:
+    r"""Pass it a filename and it will return a secure version of it.  This
+    filename can then safely be stored on a regular file system and passed
+    to :func:`os.path.join`.  The filename returned is an ASCII only string
+    for maximum portability.
+    On windows systems the function also makes sure that the file is not
+    named after one of the special device files.
+    >>> secure_filename("My cool movie.mov")
+    'My_cool_movie.mov'
+    >>> secure_filename("../../../etc/passwd")
+    'etc_passwd'
+    >>> secure_filename('i contain cool \xfcml\xe4uts.txt')
+    'i_contain_cool_umlauts.txt'
+    The function might return an empty filename.  It's your responsibility
+    to ensure that the filename is unique and that you abort or
+    generate a random filename if the function returned an empty one.
+    """
+    _filename_ascii_strip_re = re.compile(r"[^A-Za-z0-9_.-]")
+    filename = unicodedata.normalize("NFKD", filename)
+    filename = filename.encode("ascii", "ignore").decode("ascii")
+
+    for sep in os.path.sep, os.path.altsep:
+        if sep:
+            filename = filename.replace(sep, " ")
+    filename = str(_filename_ascii_strip_re.sub("", "_".join(filename.split()))).strip(
+        "._"
+    )
+
+    return filename
+
 
 class IndexHandler(tornado.web.RequestHandler):
 	def get(self):
@@ -121,7 +154,7 @@ class UploadHandler(tornado.web.RequestHandler):
 		filename = fileinfo['filename'] 
 		body = fileinfo['body'] 
 		logging.info("filename={}".format(filename))
-		filepath = os.path.join(UPLOAD_DIR, werkzeug.utils.secure_filename(filename))
+		filepath = os.path.join(UPLOAD_DIR, secure_filename(filename))
 		logging.info("filepath={}".format(filepath))
 
 		try:
