cdbf65e7f44392100183a25fd2295ea88ae4b41e
Bruno Binet <bruno.binet@gmail.com>
2022-07-07 14:31:31 +0200

    Simplify and refactor code

--- tornado/upload.py
+++ tornado/upload.py
@@ -1,193 +1,70 @@
 # -*- coding: utf-8 -*-
 import os
-import sys
-from datetime import datetime
 import tornado.httpserver
 import tornado.ioloop
-import tornado.options
 import tornado.web
 import logging
-import json
-import magic
 import re
 import unicodedata
 
-from tornado.options import define, options
+from tornado.options import define, options, parse_command_line
 
 define("port", default=8080, help="run on the given port", type=int)
 
 UPLOAD_DIR = os.path.join(os.getcwd(), "uploaded")
 logging.info("UPLOAD_DIR={}".format(UPLOAD_DIR))
-if (os.path.exists(UPLOAD_DIR) == False):
-	logging.warning("UPLOAD_DIR [{}] not found. Create this".format(UPLOAD_DIR))
-	os.mkdir(UPLOAD_DIR)
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-def secure_filename(filename: str) -> str:
-    r"""Pass it a filename and it will return a secure version of it.  This
-    filename can then safely be stored on a regular file system and passed
-    to :func:`os.path.join`.  The filename returned is an ASCII only string
-    for maximum portability.
-    On windows systems the function also makes sure that the file is not
-    named after one of the special device files.
-    >>> secure_filename("My cool movie.mov")
-    'My_cool_movie.mov'
-    >>> secure_filename("../../../etc/passwd")
-    'etc_passwd'
-    >>> secure_filename('i contain cool \xfcml\xe4uts.txt')
-    'i_contain_cool_umlauts.txt'
-    The function might return an empty filename.  It's your responsibility
-    to ensure that the filename is unique and that you abort or
-    generate a random filename if the function returned an empty one.
-    """
-    _filename_ascii_strip_re = re.compile(r"[^A-Za-z0-9_.-]")
-    filename = unicodedata.normalize("NFKD", filename)
-    filename = filename.encode("ascii", "ignore").decode("ascii")
+def normalize_path(path: str) -> str:
+    return re.sub(r"[^A-Za-z0-9_./-]", "", 
+            unicodedata.normalize("NFKD", path)
+                .encode("ascii", "ignore").decode("ascii")
+            ).strip("._")
 
-    for sep in os.path.sep, os.path.altsep:
-        if sep:
-            filename = filename.replace(sep, " ")
-    filename = str(_filename_ascii_strip_re.sub("", "_".join(filename.split()))).strip(
-        "._"
-    )
-
-    return filename
-
-
-class IndexHandler(tornado.web.RequestHandler):
-	def get(self):
-		logging.info("IndexHandler:get")
-		files = []
-		dirs = []
-		meta = {
-			"current_directory": UPLOAD_DIR
-		}
-
-		for (dirpath, dirnames, filenames) in os.walk(UPLOAD_DIR):
-			logging.info("filenames={}".format(filenames))
-			for name in filenames:
-				nm = os.path.join(dirpath, name).replace(UPLOAD_DIR, "").strip("/").split("/")
-				logging.info("nm={}".format(nm))
-				# Skip if the file is in a subdirect
-				# nm=['templates', 'index.html']
-				if len(nm) != 1: continue
-
-				fullpath = os.path.join(dirpath, name)
-				logging.info("fullpath={}".format(fullpath))
-				if os.path.isfile(fullpath) == False: continue
-				size = os.stat(fullpath).st_size
-
-				mime = magic.from_file(fullpath, mime=True)
-				logging.info("mime={}".format(mime))
-				visible = "image/" in mime
-				if (visible == False):
-					visible = "text/" in mime
-				logging.info("visible={}".format(visible))
-
-				files.append({
-					"name": name,
-					"size": str(size) + " B",
-					"mime": mime,
-					"fullname": fullpath,
-					"visible": visible
-				})
-
-		self.render("index.html", files=sorted(files, key=lambda k: k["name"].lower()), folders=dirs, meta=meta)
-		#self.write("Hello World")
-
-class DownloadHandler(tornado.web.RequestHandler):
-	def get(self):
-		filename = self.get_argument('filename', default=None)
-		logging.info("{}:filename={}".format(sys._getframe().f_code.co_name, filename))
-		if os.path.isfile(filename):
-			if os.path.dirname(filename) == UPLOAD_DIR.rstrip("/"):
-				self.set_header('Content-Type', 'application/octet-stream')
-				self.set_header('Content-Disposition', 'attachment; filename=' + os.path.basename(filename))
-				with open(filename, 'rb') as f:
-					while True:
-						data = f.read(4096)
-						if not data: break
-						self.write(data)
-				self.finish()
-			else:
-				return render_template("no_permission.html")
-		else:
-			return render_template("not_found.html")
-
-class ImageviewHandler(tornado.web.RequestHandler):
-	def get(self):
-		filename = self.get_argument('filename', default=None)
-		logging.info("{}:filename={}".format(sys._getframe().f_code.co_name, filename))
-		rotate = self.get_argument('rotate', default=0)
-		logging.info("{}:rotate={}{}".format(sys._getframe().f_code.co_name, rotate, type(rotate)))
-		if (type(rotate) is str): 
-			rotate = int(rotate)
-			logging.info("{}:rotate={}{}".format(sys._getframe().f_code.co_name, rotate, type(rotate)))
-
-		mime = magic.from_file(filename, mime=True)
-		mime = mime.split("/")
-		logging.info("mime={}".format(mime))
-
-		if (mime[0] == "image"):
-			logging.info("filename={}".format(filename))
-			filename = os.path.basename(filename)
-			logging.info("filename={}".format(filename))
-			filename = os.path.join("/uploaded", filename)
-			logging.info("filename={}".format(filename))
-			self.render("view.html", user_image = filename, rotate=rotate)
-
-		if (mime[0] == "text"):
-			contents = ""
-			f = open(filename, 'r')
-			datalist = f.readlines()
-			for data in datalist:
-				logging.debug("[{}]".format(data.rstrip()))
-				contents = contents + data.rstrip() + "<br>"
-			self.write(contents)
+def is_safe_path(basedir, path, follow_symlinks=True):
+    # resolves symbolic links
+    if follow_symlinks:
+        matchpath = os.path.realpath(path)
+    else:
+        matchpath = os.path.abspath(path)
+    return basedir == os.path.commonpath((basedir, matchpath))
 
 class UploadHandler(tornado.web.RequestHandler):
-	def post(self):
-		logging.info("UploadHandler:post")
-		logging.info("self.request={}".format(self.request))
-
-		fileinfo = self.request.files['upfile'][0]
-		#logging.info("fileinfo={}".format(fileinfo))
-		filename = fileinfo['filename'] 
-		body = fileinfo['body'] 
-		logging.info("filename={}".format(filename))
-		filepath = os.path.join(UPLOAD_DIR, secure_filename(filename))
-		logging.info("filepath={}".format(filepath))
-
-		try:
-			f = open(filepath, "wb")
-			f.write(body)
-			f.close()
-			logging.info("{} uploaded {}, saved as {}".format(self.request.remote_ip, filename, filepath))
-			responce = {"result": "upload OK"}
-		except IOError as e:
-			logging.error("Failed to write file due to IOError %s", str(e))
-			responce = {"result": "upload FAIL"}
-
-		self.write(responce)
+    def post(self):
+        if "upfile" not in self.request.files:
+            raise tornado.web.HTTPError(400)
+        upfile = self.request.files['upfile'][0]
+        filename = upfile['filename'] 
+        filepath = normalize_path(os.path.normpath(os.path.join(
+            UPLOAD_DIR, self.get_body_argument("updir", default=""), filename)))
+        if not is_safe_path(UPLOAD_DIR, filepath):
+            logging.error("UPLOAD_DIR=%s filepath=%s", UPLOAD_DIR, filepath)
+            raise tornado.web.HTTPError(400)
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
+        try:
+            with open(filepath, "wb") as f:
+                f.write(upfile['body'])
+            logging.info("{} uploaded {}, saved as {}".format(self.request.remote_ip, filename, filepath))
+            self.write({"result": "upload OK"})
+        except OSError as e:
+            logging.error("Failed to write file due to OSError %s", str(e))
+            self.write({"result": "upload FAIL"})
+            raise
 
 def make_app():
-	return tornado.web.Application(
-		handlers=[
-			(r'/uploaded/(.*)', tornado.web.StaticFileHandler, {'path': "uploaded"}),
-			(r'/static/(.*)', tornado.web.StaticFileHandler, {'path': "static"}),
-			(r"/", IndexHandler),
-			(r"/download", DownloadHandler),
-			(r"/imageview", ImageviewHandler),
-			(r"/upload_multipart", UploadHandler),
-		],
-		template_path=os.path.join(os.getcwd(), "templates"),
-		#static_path=os.path.join(os.getcwd(), "statics"),
-		debug=True)
+    return tornado.web.Application(
+        handlers=[
+            (r"/", UploadHandler),
+        ],
+        template_path=os.path.join(os.getcwd(), "templates"),
+        #debug=True,
+        )
 
 if __name__ == "__main__":
-	tornado.options.parse_command_line()
-	app = make_app()
-	http_server = tornado.httpserver.HTTPServer(app)
-	http_server.listen(options.port)
-	tornado.ioloop.IOLoop.current().start()
+    options.parse_command_line()
+    app = make_app()
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
 
 
